752a776f7205309d7dd127dbead980a29398cf3f
Summer <72228259+SummerinTech@users.noreply.github.com>
2021-07-27 21:05:04 -0700

    added copyright to page with javascript

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// Creating img element for cat photo
 let img = document.createElement("img");
 
+// Cat photo API call
 function fetchCat() {
 	let response = fetch("https://api.thecatapi.com/v1/images/search")
 		.then((res) => res.json())
@@ -8,3 +10,10 @@ function fetchCat() {
 			document.querySelector(".img-container").appendChild(img);
 		});
 }
+
+// Copyright for footer
+let date = new Date();
+let year = date.getFullYear();
+
+let footer = document.querySelector("footer");
+footer.innerText = `Copyright ${year} 😻 `;
